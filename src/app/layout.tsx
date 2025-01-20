fd325df56509aed876cb05b68ed3ffc83a41220f
kaneviggers <hello@kaneviggers.com>
2025-01-20 19:08:43 +1300

    removed unused imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Menu } from "@/components/menu";
-import { AnimatePresence, motion } from "framer-motion";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
